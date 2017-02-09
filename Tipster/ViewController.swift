50485b0e1f6085c6be9aa374748bfe76d3b64c49
Arthur Burgin <arb4778@rit.edu>
2017-02-08 21:13:12 -0500

    complete persisting values and  app life time check when restarting app

--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -16,21 +16,25 @@ class TCViewController: UIViewController {
     @IBOutlet var tipControl: UISegmentedControl!
     let defaults = UserDefaults.standard
     let tipPercentage = [18, 20, 25]
-    var savedTip = 0
-    var bill = 0.0
-    var tip = 0.0
-    var total = 0.0
-    
-    /////////////////////persist field, tip and total/////////////////////////////////////////////////////////
+    var savedTip:Int!
+    var minutes:Int!
+    var rawTime:Int!
+    var bill:Double!
+    var tip:Double!
+    var total:Double!
+    var startTime = Date()
+    var currTime = NSDate()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("view did load")
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        checkAppLifeTime()
         
         if billField.text != ""{
             if defaults.bool(forKey: "toggleValue"){
@@ -38,9 +42,16 @@ class TCViewController: UIViewController {
             }else{
                 calcTotal(val: tipPercentage[tipControl.selectedSegmentIndex])
             }
+        }else if((defaults.object(forKey: "totalVal")) != nil){
+            if (defaults.double(forKey: "billVal") == 0.0){
+                billField.text = ""
+            }else{
+                billField.text = String(defaults.double(forKey: "billVal"))
+            }
+            tipLabel.text = defaults.string(forKey: "tipVal")
+            totalLabel.text = defaults.string(forKey: "totalVal")
+            tipControl.selectedSegmentIndex = defaults.integer(forKey: "segmentVal")
         }
-        //if theres a value in the text field and theres a saved value then set the tip
-        print("view will appear")
         
     }
     
@@ -52,6 +63,43 @@ class TCViewController: UIViewController {
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        
+        saveVals(bill: bill, tip: tipLabel.text!, total: totalLabel.text!)
+    }
+    
+    //persists values from each field
+    func saveVals(bill: Double, tip: String, total: String){
+        defaults.set(bill, forKey: "billVal")
+        defaults.set(tip, forKey: "tipVal")
+        defaults.set(total, forKey: "totalVal")
+        defaults.set(tipControl.selectedSegmentIndex, forKey: "segmentVal")
+    }
+    
+    //perform a time check to see if app was last used in 10 minutes
+    func checkAppLifeTime(){
+        if(defaults.object(forKey: "startTime") != nil){
+            rawTime = Int(currTime.timeIntervalSince(defaults.object(forKey: "startTime") as! Date))
+            minutes = (rawTime/60)
+            
+            if minutes >= 10{
+                defaults.setValue(startTime, forKey: "startTime")
+                resetBill()
+            }
+        }else{
+            defaults.setValue(startTime, forKey: "startTime")
+        }
+    }
+    
+    //reset all values except default tip percentage
+    func resetBill(){
+        bill = 0.0
+        tip = 0.0
+        total = 0.0
+        defaults.set(bill, forKey: "billVal")
+        defaults.set(bill, forKey: "tipVal")
+        defaults.set(bill, forKey: "totalVal")
+        defaults.set(false, forKey: "toggleValue")
+        calcTotal(val: 0)
     }
     
     //calculates tip using the segment control changes
